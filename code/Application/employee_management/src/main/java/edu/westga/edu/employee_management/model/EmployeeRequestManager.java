8879350bbf416d463c6d0cfe4eed4a7510107b6f
dharpe11 <dharpe11@dharpe11@my.westga.edu>
2022-04-05 22:47:28 -0400

    Added Functionality to display the current list of Requests
    Created EmployeeRequestsPage UI where the employee can see their
    currently submitted work requests. Created
    EmployeeRequestsPageController class that holds the functionality for
    displaying the list of requests for the current employee. Added
    functionality to populate fields when Request is selected in listview.

---
+++ code/Application/employee_management/src/main/java/edu/westga/edu/employee_management/model/EmployeeRequestManager.java
@@ -0,0 +1,40 @@
+package edu.westga.edu.employee_management.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeRequestManager {
+	private List<EmployeeRequest> currentRequests;
+	private int numberOfRequests;
+	
+	public EmployeeRequestManager() {
+		
+		this.currentRequests = new ArrayList<EmployeeRequest>();
+		this.currentRequests.add(new EmployeeRequest("Personal Time", "04/21/2022", "04/26/2022", "PENDING"));
+		this.currentRequests.add(new EmployeeRequest("Sick Leave", "02/04/2022", "02/05/2022", "APPROVED"));
+		
+		this.numberOfRequests = 0;
+	}
+
+	public List<EmployeeRequest> getCurrentRequests() {
+		return this.currentRequests;
+	}
+	
+	public int getNumberOfRequests() {
+		return this.numberOfRequests;
+	}
+
+	public void setNumberOfRequests(int numberOfRequests) {
+		this.numberOfRequests = numberOfRequests;
+	}
+	
+	public boolean addEmployeeRequest(EmployeeRequest newRequest) {
+		if (newRequest == null) {
+			throw new IllegalArgumentException("Request cannot be null");
+		}
+		this.numberOfRequests += 1;
+		
+		return this.currentRequests.add(newRequest);
+	}
+
+}
