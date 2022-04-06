74ce32dd77cd90b2e67e8fa5eeefdcf3c688f7ef
briairie <birie1@my.westga.edu>
2022-04-06 08:47:48 -0400

    Added Functionality For Multiple Clock-ins in a day
    Added DaySheet to Timesheet for daily clock-ins. Changed UI to support
    new functionality.

--- code/Application/employee_management/src/main/java/edu/westga/edu/employee_management/Scenes.java
+++ code/Application/employee_management/src/main/java/edu/westga/edu/employee_management/Scenes.java
@@ -1,7 +1,7 @@
 package edu.westga.edu.employee_management;
 
 public enum Scenes {
-	LOGIN("LoginPage"), LANDINGPAGE("LandingPage"), HRLANDINGPAGE("HrLandingPage");
+	LOGIN("LoginPage"), LANDINGPAGE("LandingPage"), HRLANDINGPAGE("HrLandingPage"), DAILYTIMEPAGE("DailyTimePage");
 
 	private String filename;
 
