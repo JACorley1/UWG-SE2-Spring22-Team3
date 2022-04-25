c4946b7a20123a6b335f83dbc08cec8e1c6f6e5c
fdominguez27 <94133943+fdominguez27@users.noreply.github.com>
2022-04-25 18:55:41 -0400

    Added some tests for the EmployeeProfile Class
    Added some tests for the Employee Profile class for the to json method and from json method and and added a new test for the getTimeSheet

--- code/Application/employee_management/src/test/java/edu/westga/cs3211/employee_management/viewmodel/landing_page_view_model/TestConstructor.java
+++ code/Application/employee_management/src/test/java/edu/westga/cs3211/employee_management/viewmodel/landing_page_view_model/TestConstructor.java
@@ -11,21 +11,21 @@ import edu.westga.edu.employee_management.viewmodel.LandingPageViewModel;
 
 class TestConstructor {
 
-//	@Test
-//	void testValidConstruction() {
-//		EmployeeProfile profile = new EmployeeProfile(1, "Sophie", "", "Atelier", "example@gmail.com", "123-456-7890", true,
-//				"user name", "password");
-//		LandingPageViewModel viewmodel = new LandingPageViewModel(profile);
-//		
-//		assertAll(() -> assertEquals(profile.getEmail(), viewmodel.getEmailProperty().get()),
-//						() -> assertEquals(profile.getFirstName(), viewmodel.getFirstNameProperty().get()),
-//						() -> assertEquals(profile.getLastName(), viewmodel.getLastNameProperty().get()), 
-//						() -> assertEquals(profile.getID(), Integer.valueOf(viewmodel.getIdProperty().get())),
-//						() -> assertEquals(profile.getMiddleName(), viewmodel.getMiddleNameProperty().get()),
-//						() -> assertEquals(profile.isHR(), viewmodel.getHrViewButtonVisibleProperty().get()),
-//						() -> assertFalse(viewmodel.getClockInDisabledProperty().get()),
-//						() -> assertTrue(viewmodel.getClockOutDisabledProperty().get()),
-//						() -> assertEquals(profile.getPhone(), viewmodel.getPhoneProperty().get()));
-//	}
+	@Test
+	void testValidConstruction() {
+		EmployeeProfile profile = new EmployeeProfile(1, "Sophie", "", "Atelier", "example@gmail.com", "123-456-7890", true,
+				"user name", "password");
+		LandingPageViewModel viewmodel = new LandingPageViewModel(profile);
+		
+		assertAll(() -> assertEquals(profile.getEmail(), viewmodel.getEmailProperty().get()),
+						() -> assertEquals(profile.getFirstName(), viewmodel.getFirstNameProperty().get()),
+						() -> assertEquals(profile.getLastName(), viewmodel.getLastNameProperty().get()), 
+						() -> assertEquals(profile.getID(), Integer.valueOf(viewmodel.getIdProperty().get())),
+						() -> assertEquals(profile.getMiddleName(), viewmodel.getMiddleNameProperty().get()),
+						() -> assertEquals(profile.isHR(), viewmodel.getHrViewButtonVisibleProperty().get()),
+						() -> assertFalse(viewmodel.getClockInDisabledProperty().get()),
+						() -> assertTrue(viewmodel.getClockOutDisabledProperty().get()),
+						() -> assertEquals(profile.getPhone(), viewmodel.getPhoneProperty().get()));
+	}
 
 }
